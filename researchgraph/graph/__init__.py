fb0189836dab9c71bff45cc1e32698281bca26b6
toma tanaka <ulti4929@gmail.com>
2024-10-01 10:32:17 +0000

    Update pyproject.toml to include researchgraph and exclude ai_scientist

--- researchgraph/graph/__init__.py
+++ researchgraph/graph/__init__.py
@@ -1,4 +1,4 @@
 from .ai_integrator.main import AIIntegrator
-from .ai_scientist.main import AIscientist
+# from .ai_scientist.main import AIscientist
 
-__all__ = ["AIIntegrator", "AIscientist"]
+__all__ = ["AIIntegrator"]
