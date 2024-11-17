98f16ff1773e596e58418d5521631eb7bb98701f
toma tanaka <ulti4929@gmail.com>
2024-11-17 17:22:37 +0000

    fix: package

--- src/researchgraph/graph/__init__.py
+++ src/researchgraph/graph/__init__.py
@@ -1,3 +1,7 @@
 from .ai_integrator import AIIntegratorv1
+from .ai_integrator import ai_integratorv1_setting
 
-__all__ = ["AIIntegratorv1"]
+__all__ = [
+    "AIIntegratorv1",
+    "ai_integratorv1_setting",
+]
