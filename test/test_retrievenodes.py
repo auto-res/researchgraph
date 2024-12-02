82671a30a0f5adc4adc91808f47439d6dfb75638
toma tanaka <ulti4929@gmail.com>
2024-12-02 16:58:04 +0000

    chore: test

--- test/test_retrievenodes.py
+++ test/test_retrievenodes.py
@@ -1,3 +1,4 @@
+import os
 from typing import TypedDict
 from langgraph.graph import StateGraph
 
@@ -13,17 +14,19 @@ class State(TypedDict):
     github_file: str
 
 
+SAVE_DIR = os.environ.get("SAVE_DIR", "/workspaces/researchgraph/data")
+
+
 def test_retrieve_arxiv_text_node():
     input_variable = ["arxiv_url"]
     output_variable = ["paper_text"]
-    save_dir = "/workspaces/researchgraph/data"
     graph_builder = StateGraph(State)
     graph_builder.add_node(
         "arxivretriever",
         RetrievearXivTextNode(
             input_variable=input_variable,
             output_variable=output_variable,
-            save_dir=save_dir,
+            save_dir=SAVE_DIR,  # 環境変数から取得したSAVE_DIRを使用
         ),
     )
     graph_builder.set_entry_point("arxivretriever")
@@ -33,20 +36,17 @@ def test_retrieve_arxiv_text_node():
     state = {
         "arxiv_url": "https://arxiv.org/abs/1604.03540v1",
     }
-
     assert graph.invoke(state, debug=True)
 
 
 def test_retrieve_github_repository_node():
     input_variable = ["github_url"]
     output_variable = ["folder_structure", "github_file"]
-    save_dir = "/workspaces/researchgraph/data"
-
     graph_builder = StateGraph(State)
     graph_builder.add_node(
         "githubretriever",
         RetrieveGithubRepositoryNode(
-            save_dir=save_dir,
+            save_dir=SAVE_DIR,  # 環境変数から取得したSAVE_DIRを使用
             input_variable=input_variable,
             output_variable=output_variable,
         ),
@@ -58,4 +58,4 @@ def test_retrieve_github_repository_node():
     state = {
         "github_url": "https://github.com/adelnabli/acid?tab=readme-ov-file/info/refs"
     }
-    assert graph.invoke(state)
+    assert graph.invoke(state, debug=True)
