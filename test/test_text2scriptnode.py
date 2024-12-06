519fc3d2f41020b2a3b7311cb5c208b16a43c3ad
toma tanaka <ulti4929@gmail.com>
2024-12-06 06:04:23 +0000

    fix: fix python package

--- test/test_text2scriptnode.py
+++ test/test_text2scriptnode.py
@@ -15,15 +15,13 @@ SAVE_DIR = os.environ.get("SAVE_DIR", "/workspaces/researchgraph/data")
 
 
 def test_latex_node():
-    save_file_path = "/workspaces/researchgraph/data/new_method.py"
-
     graph_builder = StateGraph(State)
     graph_builder.add_node(
         "text2script",
         Text2ScriptNode(
             input_key=["code_string"],
             output_key=["script_save_path"],
-            save_file_path=save_file_path,
+            save_file_path=SAVE_DIR,
         ),
     )
     graph_builder.set_entry_point("text2script")
