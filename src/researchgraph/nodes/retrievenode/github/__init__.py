6b2cd68442a93f0463ae1305d47ad1cf121ee7bc
gen <gengaru617science@gmail.com>
2025-01-28 13:13:20 +0000

    Update __init__

--- src/researchgraph/nodes/retrievenode/github/__init__.py
+++ src/researchgraph/nodes/retrievenode/github/__init__.py
@@ -1,6 +1,7 @@
 from .retrieve_github_repository import RetrieveGithubRepositoryNode
 from .retrieve_code_with_devin import RetrieveCodeWithDevinNode
 from .retrieve_github_url import RetrieveGithubUrlNode
+from .retrieve_github_actions_artifacts import RetrieveGithubActionsArtifactsNode
 
 __all__ = [
     "RetrieveGithubRepositoryNode",
